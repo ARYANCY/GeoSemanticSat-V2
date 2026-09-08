38eff6726078eaeba969c4c1705de1795a1b5db9
Ronit Choudhury <ronit965choudhury@outlook.com>
2026-09-09 01:30:06 +0530

    [RONIT]: THIRD CORRECTNESS PASS : Checked Affine Transformations, RadiometricNormalizer, VectorIndex ADDED ERROR POPUPS: Added erorr popups when no data is available IMPROVED UI: Removed unncesaary maps, added individual heatmaps in spectral analysis tab

--- Desktop_App/Upgrahan2/src/GeoSemanticSat.Core/obj/Debug/net10.0/GeoSemanticSat.Core.AssemblyInfo.cs
+++ Desktop_App/Upgrahan2/src/GeoSemanticSat.Core/obj/Debug/net10.0/GeoSemanticSat.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeoSemanticSat.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01bcb298c5379c05845bb0e4e487e65eef7e6a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeoSemanticSat.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeoSemanticSat.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
